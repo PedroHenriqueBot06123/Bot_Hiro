e7fcfb70f4bd5d9461e20c03febde3bd23a62d36
PedroHenriqueBot06123 <77212398+PedroHenriqueBot06123@users.noreply.github.com>
2021-01-11 17:54:18 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,96 @@
+const help = (prefix) => {
+	return `┏━━❉ *Sobre bot* ❉━━┓
+┣⊱ *$owner*
+┣⊱ *$donasi*
+┣⊱ *$info*
+┣⊱ *$botstat*
+┣⊱ *$bug*
+┣━━❀ *Maker* ❀━━
+┣⊱ *$ocr*
+┣⊱ *$sticker*
+┣⊱ *$tts*
+┣⊱ *$toimg*
+┣⊱ *$phlogo*
+┣⊱ *$quotemaker*
+┣⊱ *$ninjalogo*
+┣⊱ *$glitch*
+┣⊱ *$text3d*
+┣━━❀ *Mexilhões mágicos* ❀━
+┣⊱ *$kapankah*
+┣⊱ *$apakah*
+┣⊱ *$bisakah*
+┣⊱ *$rate*
+┣━━❀ *Divertido* ❀━━━━
+┣⊱ *$meme*(en) 
+┣⊱ *$memeindo*
+┣⊱ *$hilih*
+┣⊱ *$fitnah*
+┣⊱ *$truth*
+┣⊱ *$dare*
+┣⊱ *$bucin*
+┣⊱ *$persengay*
+┣━━❀ *Anime* ❀━━
+┣⊱ *$loli*
+┣⊱ *$nsfwloli*
+┣⊱ *$anime*
+┣⊱ *$randomanime*
+┣⊱ *$randomhentai*
+┣⊱ *$nsfwloli*
+┣⊱ *$nsfwblowjob*
+┣⊱ *$nsfwneko*
+┣⊱ *$nsfwtrap*
+┣━━❀ *Media* ❀━━
+┣⊱ *$tiktokstalk*
+┣⊱ *$ytmp4*
+┣━━❀ *outros* ❀━━
+┣⊱ *$ping*
+┣⊱ *$shorturl*
+┣⊱ *$fototiktok*
+┣⊱ *$map*
+┣⊱ *$kbbi*
+┣⊱ *$artinama*
+┣⊱ *&infogempa*
+┣⊱ *$quotes*
+┣⊱ *$cerpen*
+┣⊱ *$lirik*
+┣⊱ *$pokemon*
+┣⊱ *$chord*
+┣⊱ *$blocklist*
+┣⊱ *$hidetag*
+┣⊱ *$wolflogo*
+┣⊱ *$lionlogo*
+┣⊱ *$ssweb*
+┣⊱ *$anjing*
+┣⊱ *$indohot*
+┣━━❀ *grupo* ❀━━
+┣⊱ *$linkgc*
+┣⊱ *$tagall*
+┣⊱ *$add* [+55xx]
+┣⊱ *&group* [buka/tutup]
+┣⊱ *&demote* [tag]
+┣⊱ *&promote* [tag]
+┣⊱ *$kick*
+┣⊱ *&listadmin*
+┣⊱ *$simih* [1/0]
+┣⊱ *$nsfw* [1/0]
+┣⊱ *&welcome* [1/0]
+┣━━❀ *bot dono * ❀━
+┣⊱ *$clearall*
+┣⊱ *$block*
+┣⊱ *$unblock*
+┣⊱ *$bc*
+┣⊱ *$leave*
+┣⊱ *$setpp*
+┣⊱ *$setprefix*
+┣━━━━━━━━━━━━━━
+┃    *🔥HIRO BOT🔥*
+┗━━━━━━━━━━━━━━
+➸Suporte Wa.me/+5562984737707
+
+`
+}
+
+exports.help = help
+
+
+<
